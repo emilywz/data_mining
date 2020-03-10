33afa8964e8ad0429efa9b183ed4535feb4a0f24
emily_wang_0_0 <51404834+emilywz@users.noreply.github.com>
2020-03-10 23:44:52 +0800

    Update pipelines.py

--- So/So/pipelines.py
+++ So/So/pipelines.py
@@ -5,23 +5,22 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
-# scrapy的图片管道类
+# scrapy imagepipeline
 from scrapy.pipelines.images import ImagesPipeline
 import scrapy
 
 
 class SoPipeline(ImagesPipeline):
-    # 重写get_media_requests()方法
+    # rewrite get_media_requests()
     def get_media_requests(self, item, info):
         yield scrapy.Request(
             url=item['img_link'],
             meta={'name':item['img_title']}
         )
 
-    # 重写file_path方法
+
     def file_path(self, request, response=None, info=None):
-        # 请求对象的meta属性 <==> Request(meta={})
-        # 请求对象的url属性 <==> Request(url='')
+        #rewrite file_path()
         filename = request.meta['name'] + '.' + request.url.split('.')[-1]
 
         return filename
